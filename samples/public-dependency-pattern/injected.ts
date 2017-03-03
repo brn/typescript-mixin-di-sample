3e4a26bab6e2406b08a9660908d388c0b6f38efd
brn <dobaw20@gmail.com>
2017-03-03 14:20:07 +0900

    fix public pattern

---
+++ samples/public-dependency-pattern/injected.ts
@@ -0,0 +1,10 @@
+import {
+  InjectTarget
+} from './inject-target';
+import {
+  Deps1Deps2ModuleImpl
+} from './deps1+deps2-module-impl';
+
+
+export class Injected extends Deps1Deps2ModuleImpl(InjectTarget) {
+}
--- samples/public-dependency-pattern/main.ts
+++ samples/public-dependency-pattern/main.ts
@@ -1,10 +1,7 @@
 import {
-  InjectTarget
-} from './inject-target';
-import {
-  Deps1Deps2ModuleImpl
-} from './deps1+deps2-module-impl';
+  Injected
+} from './injected';
 
 
-const injectTarget = new (Deps1Deps2ModuleImpl(InjectTarget));
-console.log(injectTarget.greet());
+const injected = new Injected();
+console.log(injected.greet());
